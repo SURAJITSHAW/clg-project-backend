5ef171bf7f3d858a7a3aa92b5b4c8094b007bc40
Surajit <piklu3043@gmail.com>
2023-08-20 19:28:39 +0530

    Joining done

--- routes/Product.js
+++ routes/Product.js
@@ -2,35 +2,41 @@ const exp = require("express");
 const router = exp.Router();
 const Product = require("../model/Product_mod");
 
-
 router.post("/add", async (req, res) => {
     var objpimg = req.files.pimg;
 
-    objpimg.mv("./public/product_img/"+objpimg.name, async (err) => {
+    objpimg.mv("./public/product_img/" + objpimg.name, async (err) => {
         if (err) {
             throw err;
-        }
-        else {
+        } else {
             var isobj = {
                 category: req.body.category,
                 pname: req.body.pname,
                 pprice: req.body.pprice,
                 pd: req.body.pd,
-                pimg: objpimg.name
+                pimg: objpimg.name,
             };
 
             await Product.create(isobj);
-            res.json({msg: "Product created successfully"});
+            res.json({ msg: "Product created successfully" });
         }
-    })
+    });
 });
 
 router.get("/sel", async (req, res) => {
-    
-    var data = await Product.find();
+    var data = await Product.aggregate([
+        // Join with user_info table
+        {
+            $lookup: {
+                from: "categories", // other table name
+                localField: "category", // name of users table field
+                foreignField: "_id", // name of userinfo table field
+                as: "catdata", // alias for userinfo table
+            },
+        },
+    ]);
 
     res.json(data);
-
 });
 
 module.exports = router;
