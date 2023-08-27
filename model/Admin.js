eddc2e8bfcf40d5335b1c139e713e1e38efbb6e6
Surajit <piklu3043@gmail.com>
2023-08-28 02:00:48 +0530

    authentication day-1

---
+++ model/Admin.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const catsc = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+});
+
+module.exports = mongoose.model("Admin", catsc);
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,24 +1,63 @@
 const exp = require("express");
 const bcrypt = require("bcrypt");
-
+const Admin = require("../model/Admin");
 const router = exp.Router();
+const jwt = require("jsonwebtoken");
 
 router.get("/reg", async (req, res) => {
 
-    var email = "surajtshaw@gmail.com";
+
+    var name = "surajt";
+    var email = "shaw@gmail.com";
     var password = "1234";
 
     const salt = await bcrypt.genSalt(10);
     const hp = await bcrypt.hash(password, salt);
 
-    console.log(salt);
-    console.log(hp);
+    // await Admin.create({
+    //     name: name,
+    //     email: email,
+    //     password: hp,
+    // });
 
     res.send(hp);
 });
 
-router.get("/login", (req, res) => {
-    res.send("Select Product");
+router.post("/login", async (req, res) => {
+    var e = req.body.email;
+    var p = req.body.pass;
+
+    var logindata = await Admin.findOne({ email: e});
+
+    if (logindata != null) {
+
+        bcrypt.compare(p, logindata.password, (err, result) => {
+            if (err) {
+                throw err;
+            } else {
+                if (result == true) {
+
+
+                    var udata = {
+                        aid: logindata._id,
+                        name: logindata.name
+                    }
+
+
+                    var jtoken = jwt.sign(udata, 'gmitkey');
+
+                    res.json({token: jtoken});
+
+                } else {
+                    res.json({ message: 'Invalid Login' });
+
+                }
+            }
+        })
+        
+    } else {
+        res.json({ message: 'Invalid Login' });
+    }
 });
 
 module.exports = router;
