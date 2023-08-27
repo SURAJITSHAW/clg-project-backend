41aacb0aa5b45dbb1f57834066cd8e1c75e6d8e7
Surajit <piklu3043@gmail.com>
2023-08-27 13:57:50 +0530

    delete a record and all it's related file

--- routes/Product.js
+++ routes/Product.js
@@ -2,6 +2,8 @@ const exp = require("express");
 const router = exp.Router();
 const Product = require("../model/Product_mod");
 
+const fs = require("fs");
+
 router.post("/add", async (req, res) => {
     var objpimg = req.files.pimg;
 
@@ -37,6 +39,17 @@ router.get("/sel", async (req, res) => {
     ]);
 
     res.json(data);
+});
+
+router.post("/del", async (req, res) => {
+    var id = req.body.id;
+    var tdata = await Product.findById(id);
+
+    fs.unlinkSync("./public/product_img/"+tdata.pimg);
+
+    await Product.findByIdAndDelete(id);
+
+    res.json({ msg: "Product Deleted successfully" });
 });
 
 module.exports = router;
